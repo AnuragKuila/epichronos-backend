05496b854abc502e563274b9f997fed63798aa07
Anurag <ajdjsjsjsjsjsj959@gmail.com>
2026-01-31 17:48:12 +0530

    Relax CORS to allow Swagger and deployed clients

--- app/main.py
+++ app/main.py
@@ -22,16 +22,12 @@ app = FastAPI(
 # -----------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost",
-        "http://127.0.0.1",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # -----------------------------
 # Routes
